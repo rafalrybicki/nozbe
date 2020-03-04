bd0e38638d02cb933561c3f91a767f62c3500810
Rafał Rybicki <rafarybicki@gmail.com>
2020-03-04 14:23:56 +0100

    Add style for Menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Menu from './components/Menu'
+import './App.css'
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,18 +20,20 @@ function App() {
   return (
     <Router>
       <Menu />
-      <Switch>
-        <Route path="/priority" component={Priority} exact />
-        <Route path="/inbox" component={Inbox} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/categories" component={Categories} />
-        <Route path="/calendar" component={Calendar} />
-        <Route path="/templates" component={Templates} />
-        <Route path="/team" component={Team} />
-        <Route path="/search" component={Search} />
-        <Route path="/settings" component={Settings} />
-        <Route path="/" component={Priority} />
-      </Switch>
+      <div className="main">
+        <Switch>
+          <Route path="/priority" component={Priority} exact />
+          <Route path="/inbox" component={Inbox} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/categories" component={Categories} />
+          <Route path="/calendar" component={Calendar} />
+          <Route path="/templates" component={Templates} />
+          <Route path="/team" component={Team} />
+          <Route path="/search" component={Search} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/" component={Priority} />
+        </Switch>
+      </div>
     </Router>  );
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import MenuItem from './MenuItem';
-
+import './Menu.css'
 
 class Menu extends React.Component {
   render() {
     return (
-      <div>
+      <div className="menu">
         <MenuItem icon='star' path='priority' amount={5} />
         <MenuItem icon='inbox' path='inbox' amount={1} />
         <MenuItem icon='all_inbox' path='projects' />
