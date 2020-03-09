f3a1a96d8a76b6be63313d73e96823e0e7002517
Rafał Rybicki <rafarybicki@gmail.com>
2020-03-09 12:09:46 +0100

    Add toogle feature for NewTask

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -3,11 +3,22 @@ import './StatusBar.css'
 import Icon from '@material-ui/core/Icon';
 
 function StatusBar({done, left}) {
+  const toggleNewTask = () => {
+    document.addEventListener('click', hideNewTask)
+    document.querySelector('.new-task').classList.add('show')
+  }
+  const hideNewTask = (e) => {
+    const index = e.path.findIndex((obj) => obj.className === "new-task show")
+    if(index < 0) {
+      document.removeEventListener('click', hideNewTask);
+      document.querySelector('.new-task').classList.remove('show')
+    }
+  }
   return (
     <div className="status-bar">
       <Icon>more_horiz</Icon>
       {`${done} done  :  ${left} left`}
-      <Icon>add</Icon>
+      <Icon onClick={toggleNewTask}>add</Icon>
     </div>
   );
 }
