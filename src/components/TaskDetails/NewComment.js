929733147ad2ea621898ea1a9528fafe62aaf9f4
Rafał Rybicki <rafarybicki@gmail.com>
2020-03-13 22:23:08 +0100

    Add toggling comment type

--- src/components/TaskDetails/NewComment.js
+++ src/components/TaskDetails/NewComment.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewComment.css'
 import SelectCommentType from './SelectCommentType'
 import Icon from '@material-ui/core/Icon';
 
 function NewComment(props) {
+  const [type, setType] = useState('Text')
   return (
     <div className="new-comment">
       <Icon className="close">close</Icon>
-      <SelectCommentType />
+      <SelectCommentType setType={(type) => setType(type)}/>
       <button>Save</button>
       <textarea placeholder="Add comment (ENTER)" />
-
+      type = {type}
     </div>
   );
 }
--- src/components/TaskDetails/SelectCommentType.js
+++ src/components/TaskDetails/SelectCommentType.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SelectCommentType.css'
 import Icon from '@material-ui/core/Icon';
 
 function SelectCommentType(props) {
+  const options = [
+    { value: 'Text', icon: 'subject' },
+    { value: 'Checklist', icon: 'list' },
+    { value: 'Attachment', icon: 'attach_file' },
+  ];
+  
+  const [current, setValue] = useState(options[0]);
+ 
+  const showOptions = (e) => {
+    document.querySelector('.options').classList.remove('hide')
+  }
+
+  const hideOptions = (e) => {
+    e.stopPropagation()
+    const index = e.currentTarget.getAttribute('data-id');
+    const type = e.currentTarget.getAttribute('data-value')
+    setValue(options[index]);
+    document.querySelector('.options').classList.add('hide')
+    props.setType(type)
+  }
+
   return (
-    <div className="select">
+    <div className="select" onClick={showOptions}>
       <Icon className="down">expand_more</Icon>
-      <div className="option" data-value="text">
-        <Icon>subject</Icon>
-        <span>text</span>
+      <div className="option" data-value={current.value}>
+        <Icon>{current.icon}</Icon>
+        <span>{current.value}</span>
       </div>
-      <div className="option" data-value="checklist">
-        <Icon>list</Icon>
-        <span>checklist</span>
-      </div>
-      <div className="option" data-value="attachment">
-        <Icon>attach_file</Icon>
-        <span>text</span>
+
+      <div className="options hide" >
+        {options.map((option, i) => 
+          <div 
+            className={option.value === current.value ? "option active" : "option"} 
+            data-value={option.value}
+            key={i}
+            data-id={i}
+            onClick={hideOptions}
+          >
+            <Icon>{option.icon}</Icon>
+            <span>{option.value}</span>
+          </div>
+        )}
       </div>
     </div>
   );
